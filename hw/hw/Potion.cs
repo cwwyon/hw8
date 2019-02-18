af1c867e7ba72f8b444fe4bb7ed24a9a42eb7e1a
LydiaWu <lxw7227@g,rit.edu>
2019-02-18 14:31:00 +0800

    changes

---
+++ hw/hw/Potion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    //make a class named Potion
+    class Potion
+    {
+        //set 5 fields
+        public string name;
+        public string color;
+        public float strength;
+        public string usage;
+        public int doses;
+
+        //set parameyrerized constructor
+        public Potion(string pname, string pcolor, float pstregth, string pusage, int pdoses)
+        {
+            name = pname;
+            color = pcolor;
+            strength = pstregth;
+            usage = pusage;
+            doses = pdoses;
+
+        }
+        //set a overloaded cobnstructor
+        public Potion()
+        {
+            name = "Potion";
+            color = "colorless";
+            strength = 50;
+            usage = "To cast on a person";
+            doses = 10;
+        }
+
+        //set a UserPotion to make each time using the potion, doses mius 1
+        public void UsePotion()
+        {
+
+            doses = doses - 1;
+            Console.WriteLine("Using " + name + " potion" + "...{0} doeses left", doses);
+            Console.WriteLine("");
+
+
+        }
+        //pring the data to user
+        public void PrintPotion()
+        {
+            Console.WriteLine("Potion :");
+            Console.WriteLine("  Name : " + name);
+            Console.WriteLine("  usage : " + usage);
+            Console.WriteLine("  strength :" + strength);
+            Console.WriteLine("  color :" + color);
+            Console.WriteLine("  Remaining doses :" + doses);
+            Console.WriteLine("");
+        }
+
+    }
+}
---
+++ hw/hw/Program.cs
@@ -0,0 +1,24 @@
+﻿//cheng wenwen
+//Pe7: make class for Potion
+using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // make a Potion  object "myCustomPotion"
+            Potion myCustomPotion;
+            myCustomPotion = new Potion("Cooo", "red", 60, "runfast", 8);
+            Potion defaultPotion = new Potion();
+            // print data out
+            myCustomPotion.PrintPotion();
+            defaultPotion.PrintPotion();
+            //use the Potion and Print ut out
+            myCustomPotion.UsePotion();
+            myCustomPotion.PrintPotion();
+
+        }
+    }
+}
